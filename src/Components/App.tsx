4de3657393596de225daa750cc379bc8e53fd91a
shawnmcmahon <shawnmcmahon17@gmail.com>
2022-05-06 19:12:09 -0600

    Add placeholder format for input

--- src/Components/App.tsx
+++ src/Components/App.tsx
@@ -5,7 +5,7 @@ import TimeSubmit from './TimeSubmit';
 
 const App:FunctionComponent = () => {
   return (
-    <main className="w-100 h-screen">
+    <main className="w-100 h-screen min-w-max">
       <Header />
       <TimeSubmit />
       <Results />
--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -4,7 +4,7 @@ import bityLogo from '../assets/images/logo.svg';
 
 const Header:FunctionComponent = () => {
   return (
-    <header className="bg-[#0C3042] h-28 w-full">
+    <header className="bg-[#0C3042] h-24 w-full">
       <img src={bityLogo} className="h-full p-4 pl-8" />
     </header>
   )
