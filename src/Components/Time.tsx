7632ef06563d4d8c30d74eebceb3f219956c92c2
shawnmcmahon <shawnmcmahon17@gmail.com>
2022-05-07 18:32:02 -0600

    Fix spacing on Time component

--- src/Components/Time.tsx
+++ src/Components/Time.tsx
@@ -12,21 +12,23 @@ const Time:FunctionComponent<timeProps> = ({raceTime, index}) => {
       className="bg-[#FFFFFF] h-12 w-64 m-4 flex flex-col justify-center rounded-sm font-proximaNovaRegular"
       key={index}
     >
-      <div>
+      <section>
         <p className="text-left text-md text-[#000000] pl-4 pt-4">{raceTime}</p>
-      </div>
-      <div className="flex h-full">
+      </section>
+      <section className="flex h-full">
         <div className="bg-[#FFFFFF] w-2/4 pl-4 pb-2">   
           <p className="text-left text-sm text-[#EB5821]">+01.03</p>
         </div>
         <div className="flex justify-end bg-[#FFF] w-2/4 pr-2 pb-2">   
-          <p className="flex text-right text-md pr-2 text-[#000000]">1</p>
+          <p className="flex text-right text-lg pr-2 text-[#000000]">
+            {(index + 1)}
+          </p>
           <img 
             className="h-6"
             src={runner} 
           />
         </div>
-      </div>
+      </section>
     </article>
   )
 }
