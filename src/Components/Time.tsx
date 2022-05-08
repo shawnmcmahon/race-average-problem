5e01b682ba503b8045dbd2dcec4d87e1fe217028
shawnmcmahon <shawnmcmahon17@gmail.com>
2022-05-07 18:27:21 -0600

    Add runner image time to map

--- src/Components/Time.tsx
+++ src/Components/Time.tsx
@@ -1,4 +1,5 @@
 import React, { FunctionComponent } from 'react'
+import runner from '../assets/images/run.svg';
 
 interface timeProps {
   raceTime: string, 
@@ -8,11 +9,24 @@ interface timeProps {
 const Time:FunctionComponent<timeProps> = ({raceTime, index}) => {
   return (
     <article 
-      className="bg-[#FFFFFF] h-16 w-64 m-4 flex flex-col justify-center rounded-sm font-proximaNovaRegular"
+      className="bg-[#FFFFFF] h-12 w-64 m-4 flex flex-col justify-center rounded-sm font-proximaNovaRegular"
       key={index}
     >
-      <p className="text-left pt-2 pl-2 text-md text-[#000000]">{raceTime}</p>
-      <p className="text-left pb-2 pl-2 text-sm text-[#EB5821]">+01.03</p>
+      <div>
+        <p className="text-left text-md text-[#000000] pl-4 pt-4">{raceTime}</p>
+      </div>
+      <div className="flex h-full">
+        <div className="bg-[#FFFFFF] w-2/4 pl-4 pb-2">   
+          <p className="text-left text-sm text-[#EB5821]">+01.03</p>
+        </div>
+        <div className="flex justify-end bg-[#FFF] w-2/4 pr-2 pb-2">   
+          <p className="flex text-right text-md pr-2 text-[#000000]">1</p>
+          <img 
+            className="h-6"
+            src={runner} 
+          />
+        </div>
+      </div>
     </article>
   )
 }
