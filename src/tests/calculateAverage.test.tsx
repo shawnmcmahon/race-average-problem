2415cc45578e495a9253bf37127556ba94edb4fd
shawnmcmahon <shawnmcmahon17@gmail.com>
2022-05-10 15:40:21 -0600

    Add unit test for test case #2

--- src/tests/calculateAverage.test.tsx
+++ src/tests/calculateAverage.test.tsx
@@ -1,9 +1,19 @@
 import calculateAverage from '../util/calculateAverage'
 
-
-
 test('calculateAverage function should return correct value', () => {
   const average = calculateAverage([1, 2, 3])
   expect(average).toBe(2)
+});
+
+test('calculateAverage should return the correct value for test case #1', () => {
+  const average = calculateAverage([240, 241])
+  console.log(average)
+  expect(average).toBe(27239)
+});
+
+test('calculateAverage should return the correct value for test case #3', () => {
+  const average = calculateAverage([26280, 27720, 27718])
+  console.log(average)
+  expect(average).toBe(27239)
 });
 
