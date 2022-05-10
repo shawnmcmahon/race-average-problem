3ac8559a319c56ee1414d0c59b47fb61f68f2ecc
shawnmcmahon <shawnmcmahon17@gmail.com>
2022-05-09 22:01:56 -0600

    Add refactored logic via calculateTime.tsx

---
+++ src/util/calculateTime.tsx
@@ -0,0 +1,44 @@
+const calculateTime = (hours: number, minutes: number, period: string, days: number) => {
+  let totalTime = 0;
+
+  // Count the amount of hours
+  if (hours === 12 && period === 'PM' && days === 1 ) {
+    totalTime += hours
+  } else if (hours !== 12 && period === 'PM' && days === 1) {
+    totalTime += hours
+  } else if (period === 'AM' && days === 1) {
+    totalTime += (hours + 12)
+  }
+
+  // Check for edge cases for races that start outside day 1
+  if (hours === 12 && period === 'AM' && days > 1) {
+    totalTime += 24
+  } else if (hours === 12 && period === 'PM' && days > 1) {
+    totalTime += 36
+  } else if (hours !== 12 && period === 'PM' && days > 1) {
+    totalTime += (hours + 36)
+  } else if (hours !== 12 && period === 'AM' && days > 1) {
+    totalTime += (hours + 24)
+  }
+
+  // Add a full day for each time longer than 2 days
+  if (days > 2) {
+    totalTime += ((days - 2) * 24)
+  }
+
+  console.log('total time', totalTime)
+
+  // Convert to minutes
+  totalTime *= 60 
+  // Add minutes from original time
+  totalTime += minutes 
+  // Account for the start of the race at 8am by subtracting 8 hours
+  totalTime -= 480
+
+  console.log(totalTime)
+  
+  return totalTime
+  
+}
+
+export default calculateTime
